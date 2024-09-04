2136d3a1ea0c8487b4fc7d9494381408e91cf22b
trumk <nguyentrungtest2292003@gmail.com>
2024-09-04 09:56:42 +0700

    connect mongo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const mongoose = require("mongoose");
+const app = express();
 
-
-
-
-const hostname = 'localhost'
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-    console.log(`hello nxt, server http://${hostname}:${port}/`)
+mongoose.connect("mongodb+srv://nguyentrung2292003:GYCKF3LGpSWUfabU@databases.l0xs9.mongodb.net/Final-project")
+  .then(() => {
+    console.log("Connected to mongodb!");
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
   })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
