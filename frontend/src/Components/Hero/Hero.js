043d416a24500d49e3fae9464bc3919b054abaca
trumk <nguyentrungtest2292003@gmail.com>
2024-11-09 10:29:59 +0700

    update hero component

--- frontend/src/Components/Hero/Hero.js
+++ frontend/src/Components/Hero/Hero.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import './style.css'; 
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+const images = [
+  "/imgs/gre.jpg",
+  "/imgs/gre2.jpg",
+  "/imgs/gre3.jpg",
+];
 
 const Hero = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const imageInterval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 5000); 
+
+    return () => clearInterval(imageInterval); 
+  }, []);
+
   return (
-    <section className="hero">
+    <section
+      className="hero"
+      style={{ backgroundImage: `url(${images[currentImage]})` }}
+    >
       <h1>Welcome to Gree Project</h1>
       <p>Explore the best projects here</p>
-      <button>Explore Now</button>
+      <button>
+        <Link className="nav-link" to="/projects">
+          Explore Now
+        </Link>
+      </button>
     </section>
   );
 };
